2bb2217f83db3b0d4466762b26fcaa5a8c32fa80
Am56blard <jm.amblard1@gmail.com>
2025-03-07 12:24:25 +0100

    MAJ page SignIn

--- src/pages/auth/SigninPage.tsx
+++ src/pages/auth/SigninPage.tsx
@@ -3,8 +3,8 @@ export const SigninPage = () => {
 
     return (
 <>
-
-        <div className="bg-green">
+        <img src="../public/logo.png" alt="Logo Balance ton job" className="h-50 m-auto"/>
+        <div className="bg-green-300">
            
              <h1 className="font-[var(--font-nunito)] text-center text-6xl bg-gradient-to-r from-cyan-700 to-blue-950 bg-clip-text text-transparent bord "> INSCRIPTION </h1>
             
