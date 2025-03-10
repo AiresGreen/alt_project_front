9591c70c4d0e37c6977a8a133fba7e9238556648
Am56blard <jm.amblard1@gmail.com>
2025-03-10 15:04:25 +0100

    Formulaire en place, reste à le designer

--- src/pages/auth/SigninPage.tsx
+++ src/pages/auth/SigninPage.tsx
@@ -1,42 +1,132 @@
-import { Input } from "@/components/ui/input";
-import { useState } from "react";
-import { LoginInterface } from "@/interface/LoginInterface";
 
-export const SigninPage = () => {
+  import { useState } from "react";
+  import { LoginInterface } from "@/interface/LoginInterface";
 
-const[name,setName]=useState<string>("");
-const[firstname,setFirstname]=useState<string>("");
-const[email,setEmail]=useState<string>("");
-const[password,setPassword]=useState<string>("");
-const[passwordconfirmation,setPasswordconfirmation]=useState<string>("");
+  import { Button } from "@/components/ui/button";
+  import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+  } from "@/components/ui/form"
+  import { Input } from "@/components/ui/input"
 
-    const handleSubmit=(e)=>{
-    e.preventDefault();
-    const data : LoginInterface={
-        name:name,
-        firstName:firstname,
-        email:email,
-        password:password,
-        passwordconfirmation:passwordconfirmation,
-    };
-}
-    return (
-<>
-        <img src="../public/logo.png" alt="Logo Balance ton job" className="h-50 m-auto"/>
-        <div className="bg-green-300">
-           
-             <h1 className="font-[var(--font-nunito)] text-center text-6xl bg-gradient-to-r from-cyan-700 to-blue-950 bg-clip-text text-transparent bord "> INSCRIPTION </h1>
-            <form className="flex flex-col w-1/3 gap-2.5" onSubmit={handleSubmit}>
-                <Input
-                type="string"
-                placeholder="Votre Nom ?"
-                onChange={(e)=>setName(e.target.value)}
-                value={login}
-                />
+  import { zodResolver } from "@hookform/resolvers/zod"
+  import { useForm } from "react-hook-form"
+  import { z } from "zod"
+  
+//Regex qui va être appelé et servira pour valider le mot de passe
+  const passwordValidation = new RegExp(
+    /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
+  );
 
-            </form>
+  const formSchema = z.object({
+    username: z.string().min(2).max(50),
+    lastname:z.string().min(2).max(50),
+    email:z.string().min(2).email(),
+    password: z
+    .string()
+    .min(8)
+    .refine((password) => /[A-Z]/.test(password), { message: "Password must contain at least one uppercase letter" })
+    .refine((password) => /[a-z]/.test(password), { message: "Password must contain at least one lowercase letter" })
+    .refine((password) => /[0-9]/.test(password), { message: "Password must contain at least one digit" })
+    .refine((password) => /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(password), { message: "Password must contain at least one special character" })
+    });
+    
+  
 
-        </div>
-</>
-    );
-}
+
+
+  export const SigninPage = () => {
+
+    const form = useForm<z.infer<typeof formSchema>>({
+      resolver: zodResolver(formSchema),
+      defaultValues: {
+        username: "",
+        lastname:"",
+        email:"",
+        password:"",
+      },
+    })
+  
+    // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof formSchema>) {
+      // Do something with the form values.
+      // ✅ This will be type-safe and validated.
+        console.log(values)
+    }
+
+      return (
+  <>
+          <img src="../public/logo.png" alt="Logo Balance ton job" className="h-50 m-auto"/>
+          <div className="bg-green-300">
+            
+              <h1 className="font-[var(--font-nunito)] text-center text-6xl bg-gradient-to-r from-cyan-700 to-blue-950 bg-clip-text text-transparent bord "> INSCRIPTION </h1>
+
+
+          </div>        
+          <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Prénom</FormLabel>
+                <FormControl>
+                  <Input placeholder="John" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="lastname"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Votre nom</FormLabel>
+                <FormControl>
+                  <Input placeholder="Malkovitch" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input type="email" placeholder="john@malkovitch.com" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Mot de Passe</FormLabel>
+                <FormControl>
+                  <Input type="password" placeholder="Mot de Passe" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          
+
+          <Button>Submit</Button>
+        </form>
+      </Form>
+  </>
+      );
+  }
